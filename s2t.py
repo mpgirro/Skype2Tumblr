293d30b83b7e08411bea5c3f49d18c4611e8ccac
C Püh <cpuehringer@gmx.at>
2011-04-08 18:55:04 +0200

    added a little parsing of messages

--- s2t.py
+++ s2t.py
@@ -1,11 +1,9 @@
 import Skype4Py as sky
 import time
-import sys
+import re
+				
 
-
-chats_to_follow = ("The High Council of Disposia")
-
-def attachment_status_change(status):
+def attachment_status_change(status):						# event handler for local attachment to skype
 
 	print "API attachment status changed to: " + client.Convert.AttachmentStatusToText(status)
 	if status == sky.apiAttachAvailable:
@@ -22,13 +20,32 @@ def attachment_status_change(status):
 	elif status == sky.apiAttachNotAvailable:
 		print "no api hook available"
 			
-def message_status_change(message, status):
+def message_status_change(message, status):					# event handler for messages
 	global chats_to_follow
 
 	if str(message.Chat.Topic) in chats_to_follow:
 		if status == 'RECEIVED' or status == 'SENT':
-			print(message.FromDisplayName + ': ' + message.Body)
-			##########message parsing code goes here##############
+			parse_message(message)
+
+
+
+def parse_message(message):
+	global key_expressions
+	
+	msg = message.Body							# get the text of the message
+	for k, v in key_expressions.iteritems():
+		m = re.search(k, msg)						# look for each of the given regular expressions in the message, if no match is found, m will be None
+		if m:
+			v(msg[m.start():len(msg)].strip())			# get the message body without the prefix and surrounding whitespaces and call the given method	
+			
+
+def post_on_tumblr(message):
+	pattern = "http://www.\S{1,}\.[a-zA-Z]{2,3}
+
+###############
+chats_to_follow = ("The High Council of Disposia")
+key_expressions = {"\b<tumb>\b" : post_on_tumblr}
+###############
 	
 
 print "skype2tumblr started, initializing..."
@@ -39,5 +56,5 @@ client.OnMessageStatus = message_status_change;					# set event handler for mess
 print "connecting to skype....."
 client.Attach()									# connect to api hook
 
-while True:
+while True:									# wait for new message events
 	time.sleep(1)
