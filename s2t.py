edad1704da63db0fed9b1557183c02017e8d7e92
C Püh <cpuehringer@gmx.at>
2011-04-09 18:01:56 +0200

    changed everything to a more object oriented style

--- s2t.py
+++ s2t.py
@@ -3,58 +3,67 @@ import time
 import re
 				
 
-def attachment_status_change(status):						# event handler for local attachment to skype
+class SkypeListener(object):
 
-	print "API attachment status changed to: " + client.Convert.AttachmentStatusToText(status)
-	if status == sky.apiAttachAvailable:
-		client.Attach();
-	elif status == sky.apiAttachSuccess:
-		print "successfully connected to skype"
+	def __init__(self, chats_to_follow, key_expressions = { }):
+		self.chats_to_follow = chats_to_follow
+		self.key_expressions = key_expressions
+
+		self.hook = sky.Skype()
+		self.hook.OnAttachmentStatus = self.attachment_status_change;
+		self.hook.OnMessageStatus = self.message_status_change;	
+
+	def attach(self):
+		print "connecting to skype....."
+		self.hook.Attach()
+
+	def attachment_status_change(self, status):					# event handler for local attachment to skype
+		print "API attachment status changed to: " + self.hook.Convert.AttachmentStatusToText(status)
+		if status == sky.apiAttachAvailable:
+			self.hook.Attach();
+		elif status == sky.apiAttachSuccess:
+			print "successfully connected to skype"
 	
-	elif status == sky.apiAttachUnknown:
-		print "*************** unknown api attachment status *****************" 
-	elif status == sky.apiAttachPendingAuthorization:
-		print "api authorization pending, please authorize this script in skype to listen to incoming messages"
-	elif status == sky.apiAttachRefused:
-		print "api attachment refused, please authorize this script in skype to listen to incoming messages"
-	elif status == sky.apiAttachNotAvailable:
-		print "no api hook available"
-			
-def message_status_change(message, status):					# event handler for messages
-	global chats_to_follow
-
-	if str(message.Chat.Topic) in chats_to_follow:
-		if status == 'RECEIVED' or status == 'SENT':
-			parse_message(message)
+		elif status == sky.apiAttachUnknown:
+			print "*************** unknown api attachment status *****************" 
+		elif status == sky.apiAttachPendingAuthorization:
+			print "api authorization pending, please authorize this script in skype to listen to incoming messages"
+		elif status == sky.apiAttachRefused:
+			print "api attachment refused, please authorize this script in skype to listen to incoming messages"
+		elif status == sky.apiAttachNotAvailable:
+			print "no api hook available"
 
 
+	def message_status_change(self, message, status):				# event handler for messages
+		if str(message.Chat.Topic) in self.chats_to_follow:
+			if status == 'RECEIVED' or status == 'SENT':
+				self.parse_message(message)
+		
 
-def parse_message(message):
-	global key_expressions
-	
-	msg = message.Body							# get the text of the message
-	for k, v in key_expressions.iteritems():
-		m = re.search(k, msg)						# look for each of the given regular expressions in the message, if no match is found, m will be None
-		if m:
-			v(msg[m.start():len(msg)].strip())			# get the message body without the prefix and surrounding whitespaces and call the given method	
+	def parse_message(self, message):
+		msg = message.Body							# get the text of the message
+		for k, v in self.key_expressions.iteritems():
+			m = re.search(str(k), msg)						# look for each of the given regular expressions in the message, if no match is found, m will be None
+			print msg, k, m
+			if m:
+				v(msg[m.start():len(msg)].strip())			# get the message body without the prefix and surrounding whitespaces and call the given method	
 			
 
 def post_on_tumblr(message):
-	pattern = "http://www.\S{1,}\.[a-zA-Z]{2,3}
+	pattern = "http://www.\S{1,}\.[a-zA-Z]{2,3}"
+	print "baem tumblr"
 
-###############
-chats_to_follow = ("The High Council of Disposia")
-key_expressions = {"\b<tumb>\b" : post_on_tumblr}
-###############
+
+if __name__ == "__main__":
+
+
+	chats_to_follow = ("The High Council of Disposia")
+	key_expressions = {"\b<tumb>\b" : post_on_tumblr }
 	
 
-print "skype2tumblr started, initializing..."
-client = sky.Skype()
-client.OnAttachmentStatus = attachment_status_change;				# set event handler for api hook
-client.OnMessageStatus = message_status_change;					# set event handler for message status
+	print "skype2tumblr started, initializing..."
+	listener = SkypeListener(chats_to_follow, key_expressions)	
+	listener.attach()									# connect to api hook
 
-print "connecting to skype....."
-client.Attach()									# connect to api hook
-
-while True:									# wait for new message events
-	time.sleep(1)
+	while True:										# wait for new message events
+		time.sleep(1)
