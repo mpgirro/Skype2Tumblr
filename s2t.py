a484b2c426c1ee1414eaa15b605d2283f738874b
C Püh <cpuehringer@gmx.at>
2011-04-09 20:05:58 +0200

    changed the listening manner, so that listeners can now check directly for themselves if the message is relevant and added a little code to the Tumblr class

--- s2t.py
+++ s2t.py
@@ -5,19 +5,27 @@ import re
 
 class SkypeListener(object):
 
-	def __init__(self, chats_to_follow, key_expressions = { }):
+	def __init__(self, chats_to_follow, message_listeners = ()):
+		'''creates a new SykpeListner object which will follow the chats specified in chats_to_follow (tuple) and look for 
+		the given regular expressions specified in key_expresssions (dict), if an expression was matched, the corresponding listener 
+		method given in the dictionary will be called'''
+
 		self.chats_to_follow = chats_to_follow
-		self.key_expressions = key_expressions
+		self.message_listeners = message_listeners
 
 		self.hook = sky.Skype()
 		self.hook.OnAttachmentStatus = self.attachment_status_change;
 		self.hook.OnMessageStatus = self.message_status_change;	
 
 	def attach(self):
+		'''attaches the Skype object to the local Skype api hook'''
 		print "connecting to skype....."
 		self.hook.Attach()
 
-	def attachment_status_change(self, status):					# event handler for local attachment to skype
+	def attachment_status_change(self, status):					
+		'''this is the event handler method for the local attachment to skype, it will automatically connect or reconnect to Skype if Skype
+		was shutdown and restarted or if the script was started before Skype'''
+
 		print "API attachment status changed to: " + self.hook.Convert.AttachmentStatusToText(status)
 		if status == sky.apiAttachAvailable:
 			self.hook.Attach();
@@ -34,23 +42,20 @@ class SkypeListener(object):
 			print "no api hook available"
 
 
-	def message_status_change(self, message, status):				# event handler for messages
+	def message_status_change(self, message, status):				
+		'''this is the event handler method for messages, if a new message is received or sent, it will call all listening methods'''
 		if str(message.Chat.Topic) in self.chats_to_follow:
 			if status == 'RECEIVED' or status == 'SENT':
-				self.parse_message(message)
+				for listener in self.message_listeners:
+					listener(message)
 		
 
-	def parse_message(self, message):
-		msg = message.Body							# get the text of the message
-		for k, method in self.key_expressions.iteritems():
-			m = re.search(k, msg)						# look for each of the given regular expressions in the message, if no match is found, m will be None
-			if m:
-				method(msg[m.start():len(msg)].strip())			# get the message body without the prefix and surrounding whitespaces and call the given method	
-			
 
 class Tumblr(object):
 
 	def __init__(self, account = "", password = "", accfilename = None):
+		'''creates a new Tumblr object with the given accountname and password, to not have to write the login data in here 
+		the additional option to read it from an external file is given'''
 		if accfilename:
 			accfile = open(accfilename, "r")
 			self.account = accfile.readline().strip("\n")
@@ -59,11 +64,44 @@ class Tumblr(object):
 		else:
 			self.account = account
 			self.password = password
-		print self.account, self.password
+		self.urlpattern = "(http://)?(www\.)?\S+\.[a-zA-Z]{2,3}(/(\S+/?)*)?"
+		self.expression_matches = ("^<tumb> ",)
 
-	def post(self, message):
-		#spattern = "http://www.\S{1,}\.[a-zA-Z]{2,3}"
-		print "baem tumblr"
+	def match(self, message):
+		for exp in self.expression_matches:
+			msg = message.Body		
+			m = re.search(exp, msg)		
+			if m:
+				self.post(msg[m.end():len(msg)].strip(), str(message.Sender.DisplayName))	
+		
+
+	def post(self, message, sender):	
+		'''posts the given message to a tumblog specified by account, before doing so it will parse the message for a url.
+		if a url is found, it will send it to tumblr to analyse it and collect relevant data before sending it via the html api'''
+		match = re.search(self.urlpattern, message)
+
+		url = "http://www.tumblr.com/"
+		caption = " ( by " + sender + " via Skype) "
+
+		print match, message
+		if match:
+			if match.start() > 0:
+				caption = message[:match.start()].strip() + caption
+			posturl = message[match.start():match.end()]
+			#tumblrtype = "regular"
+			#title = caption
+			#body = posturl
+		elif len(message) > 0:
+			tumblrtype = "quote"
+			quote = message
+		else:
+			print "could not find anything to post in the given message: " + message
+
+		#if apiurl:
+		#	pass
+			#open api url, else, do nothing and return
+
+	
 
 
 if __name__ == "__main__":
@@ -71,10 +109,10 @@ if __name__ == "__main__":
 	tumblr = Tumblr(accfilename="tumblr.txt")
 
 	chats_to_follow = ("The High Council of Disposia")
-	key_expressions = {"^<tumb> " : tumblr.post }
+	message_listeners = (tumblr.match,)
 
 	print "skype2tumblr started, initializing..."
-	listener = SkypeListener(chats_to_follow, key_expressions)	
+	listener = SkypeListener(chats_to_follow, message_listeners)	
 	listener.attach()									# connect to api hook
 
 	while True:										# wait for new message events
